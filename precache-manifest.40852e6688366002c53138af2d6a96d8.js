bece1083d771dbdbb4c7d39faa74f5c241e15192
usama147 <gierdienusama@gmail.com>
2021-02-18 11:50:19 +0200

    Updates

---
+++ precache-manifest.40852e6688366002c53138af2d6a96d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fb310fc4605ba12eb1d",
+    "url": "/Online-portfolio/static/css/main.124604bd.chunk.css"
+  },
+  {
+    "revision": "2fb310fc4605ba12eb1d",
+    "url": "/Online-portfolio/static/js/main.2fb310fc.chunk.js"
+  },
+  {
+    "revision": "fb2c43c22ab42e65e67d",
+    "url": "/Online-portfolio/static/js/1.fb2c43c2.chunk.js"
+  },
+  {
+    "revision": "a5c49a38132a1c5ce85a",
+    "url": "/Online-portfolio/static/js/runtime~main.a5c49a38.js"
+  },
+  {
+    "revision": "76567418b21817481fa6920cd9273aa3",
+    "url": "/Online-portfolio/static/media/CV.76567418.pdf"
+  },
+  {
+    "revision": "8eb72ea6927b25fad5ae96bd88a9e79c",
+    "url": "/Online-portfolio/static/media/python.8eb72ea6.png"
+  },
+  {
+    "revision": "e966306907cfb8c995315a1877512a3d",
+    "url": "/Online-portfolio/static/media/JavaScript-Logo.e9663069.png"
+  },
+  {
+    "revision": "c45d3ffc50d084a92ae3f40555ee946e",
+    "url": "/Online-portfolio/static/media/reactJS.c45d3ffc.png"
+  },
+  {
+    "revision": "a386f430dd7926cfca33263a6be19a76",
+    "url": "/Online-portfolio/static/media/wordpress.a386f430.png"
+  },
+  {
+    "revision": "107ba640bde2b586a6e76cf8e905ec18",
+    "url": "/Online-portfolio/static/media/css.107ba640.png"
+  },
+  {
+    "revision": "cd09c159bd4dd9f7a5715b5cc5fe4f0c",
+    "url": "/Online-portfolio/index.html"
+  }
+];
