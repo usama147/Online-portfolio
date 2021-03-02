21c70aa22b9859074f04ef12606f1b4318e9aa5b
usama147 <gierdienusama@gmail.com>
2021-03-02 13:09:14 +0200

    CV updated

---
+++ build/precache-manifest.9a836a570944f57953cd22c5a662b259.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1075978eca36cd5b8a96",
+    "url": "/Online-portfolio/static/css/main.124604bd.chunk.css"
+  },
+  {
+    "revision": "1075978eca36cd5b8a96",
+    "url": "/Online-portfolio/static/js/main.1075978e.chunk.js"
+  },
+  {
+    "revision": "fb2c43c22ab42e65e67d",
+    "url": "/Online-portfolio/static/js/1.fb2c43c2.chunk.js"
+  },
+  {
+    "revision": "a5c49a38132a1c5ce85a",
+    "url": "/Online-portfolio/static/js/runtime~main.a5c49a38.js"
+  },
+  {
+    "revision": "25b209ef025327f596012fb51b4a3504",
+    "url": "/Online-portfolio/static/media/Usama Gierdien CV.25b209ef.pdf"
+  },
+  {
+    "revision": "8eb72ea6927b25fad5ae96bd88a9e79c",
+    "url": "/Online-portfolio/static/media/python.8eb72ea6.png"
+  },
+  {
+    "revision": "e966306907cfb8c995315a1877512a3d",
+    "url": "/Online-portfolio/static/media/JavaScript-Logo.e9663069.png"
+  },
+  {
+    "revision": "c45d3ffc50d084a92ae3f40555ee946e",
+    "url": "/Online-portfolio/static/media/reactJS.c45d3ffc.png"
+  },
+  {
+    "revision": "a386f430dd7926cfca33263a6be19a76",
+    "url": "/Online-portfolio/static/media/wordpress.a386f430.png"
+  },
+  {
+    "revision": "107ba640bde2b586a6e76cf8e905ec18",
+    "url": "/Online-portfolio/static/media/css.107ba640.png"
+  },
+  {
+    "revision": "5f48cdb3ba69c32108a91077c10cb204",
+    "url": "/Online-portfolio/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,131 @@
+
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Usama Gierdien",
+    "role": "aspiring Frontend Developer",
+    "number":"063 424 4528",
+    "number2":"071 733 1601",
+    "email":"gierdienusama@gmail.com",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/usama-gierdien-77618b1ab/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/usama147",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"codepen",
+          "url":"https://codepen.io/usama147",
+          "className":"fa fa-code"
+        },
+      ],
+    "aboutme":"I’m creative, innovative and naturally curious. I strive to give my all and to persevere when faced with challenges. As a child I would always be so intrigued with TV games, I just had to know how it all worked. So, when I saw the opportunity to study coding, I went straight for it. I knew exactly what I wanted to study after high school. Technology is everchanging and I want to be a part of that everchanging world.",
+    "address":"Address - 1055 Edward Street Malmesburry Chatsworth.",
+    "education":[
+      {
+        "Education":"Life Choices Coding Bootcamp",
+        "YearOfStarting":"2020-",
+        "YearOfPassing":"2020",
+        
+      },
+      {
+        "Education":"Belgravia High School",
+        "YearOfStarting":"2015-",
+        "YearOfPassing":"2019",
+        
+      },
+    ],
+    "work":[
+        {
+          "CompanyName":"LC Studios",
+          "specialization":"Front End (Wordpress)",
+          "Start":"November 2020",
+          "Achievements":""
+        },
+    ],
+    
+    "skills":[
+      {
+        "skillname":"HTML5",
+        "icon":"fa fa-html5"
+      },
+      {
+        "skillname":"CSS",
+        "icon":"fa fa-css3"
+      },
+      {
+        "skillname":"javascript",
+        "icon":"fa fa-js-square"
+      },
+      {
+        "skillname":"python",
+        "icon":"fa fa-python"
+      }, 
+      {
+        "skillname":"react",
+        "icon":"fa fa-github"
+      },
+      {
+        "skillname":" wordpress",
+        "icon":"fa fa-github"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Github Finder",
+        "description":"Used to search for github profiles",
+        "imgurl":"https://i.ibb.co/dg9YdvW/github-finder.png",
+        "link":"https://github-finder-systems.netlify.app/",
+        "repo":"https://github.com/usama147/Github-Finder-Project"
+          
+      },
+      {
+        "name":"Python Lotto Programme",
+        "description":"Lotto programme",
+        "imgurl":"https://i.ibb.co/PYD34jy/lotto.png",
+        "link":"https://repl.it/talk/share/Lotto-Programme/120330",
+        "repo":"https://github.com/usama147/LottoGame"
+      },
+      {
+        "name":"Ecommerce Website",
+        "description":"Ecom site",  
+        "imgurl":"https://i.ibb.co/vJ8LDgq/Ecom.png",
+        "link":"https://usama147.github.io/Ecom-website/",
+        "repo":"https://github.com/usama147/Ecom-website"
+        
+      },
+      {
+        "name":"Basic Calculator",
+        "description":"Calculator app",
+        "imgurl":"https://i.ibb.co/D4sfWK4/calc.png",
+        "link":"https://calculator-app-system.netlify.app/",
+        "repo":"https://github.com/usama147/Basic-Calculator"
+        
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Usama has grown to be both a curious and resourceful developer, with a keen interest in front end, which he is constantly striving to improve. He works well both alone, and in a team, and tackles his work with diligence.",
+        "name":"Jason Wandrag"
+      },
+      {
+        "description":"It gives me great honor to know that I have worked with Usama.He is a inspirational man. One who never lets down his team. Always  searching ways to prosper.I would definitely recommend  Usama to any workforce. He has the skill set and abilities.",
+        "name":"Jaabir Esau "
+      },
+      {
+        "description":"Usama is one of the most hardworking learners at life choices academy , he always makes sure that he does his best and nothing but the best.",
+        "name":"Imtyaaz Temoore"
+      },
+      {
+        "description":"If you're looking for a hardworking and helpful employee, Usama Gierdien is your man.His willing to help wherever he is required , he is very talented when it comes to his work he always puts in 100% effort.",
+        "name":"Zubair De vries"
+      }
+    ]
+  }
+  
+  export default resumeData
